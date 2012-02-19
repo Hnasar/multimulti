f3af56b8c42148efa0141b6cb233c68514f136b4
Hashem Nasarat <Hashem.nasarat@tufts.edu>
2012-02-19 18:57:22 -0500

    Added woozy (1 directional turning snake)

---
+++ woozy/main.lua
@@ -0,0 +1,63 @@
+local speed = 5
+local xdirection, ydirection = 0, speed
+local xpos, ypos = display.contentWidth / 2, display.contentHeight / 8
+local radius = 20
+local circle = display.newCircle( xpos, ypos, radius )
+circle:setFillColor(255, 255, 255)
+local direction = "left"
+
+local function touchScreen(event)
+  if event.phase == "began" then
+    if direction == "left" then
+      if ydirection > 0 then
+        xdirection = speed
+        ydirection = 0
+      elseif ydirection < 0 then
+        xdirection = -speed
+        ydirection = 0
+      elseif xdirection > 0 then
+        xdirection = 0
+        ydirection = -speed
+      elseif xdirection < 0 then
+        xdirection = 0
+        ydirection = speed
+      end
+    end
+  end
+end
+Runtime:addEventListener("touch", touchScreen)
+
+
+local tPrevious = system.getTimer()
+local function animate(event)
+  local tDelta = event.time - tPrevious
+  tPrevious = event.time
+  xpos = xpos + (0.084*xdirection*tDelta)
+  ypos = ypos + (0.066*ydirection*tDelta)
+
+  if (xpos > display.contentWidth - 20 or xpos < 20) then
+    xdirection = xdirection * -1
+  end
+  if (ypos > display.contentHeight - 20 or ypos < 20) then
+    ydirection = ydirection * -1
+  end
+  circle:translate(xpos - circle.x, ypos - circle.y)
+
+  --TODO
+  if not system.getTimer() % 5000 then 
+    print "switching"
+  end
+end
+Runtime:addEventListener("enterFrame", animate)
+
+local tSuspend
+local function onSuspendResume (event)
+  if "applicationSuspend" == event.type then
+    tSuspend = system.getTimer()
+  elseif "applicationResume" == event.type then
+    tPrevious = tPrevious + ( system.getTimer() - tSuspend )
+  end
+end
+Runtime:addEventListener("system", onSuspendResume)
+
+
